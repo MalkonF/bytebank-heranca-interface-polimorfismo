10987f69c02a15055b40f39a8eef6d30222de858
MalkonF <sudo777@protonmail.com>
2021-06-23 22:02:42 -0300

    Criando classe funcionário

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,15 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    testaLacos()
+    val alex = Funcionario(
+        nome = "Malkon",
+        cpf = "111.111.111-11",
+        salario = 1000.00
+    )
+
+    println("Nome: ${alex.nome}")
+    println("Cpf: ${alex.cpf}")
+    println("Salário: ${alex.salario}")
+    println("Bonificação: ${alex.bonificacao()}")
 }
 
 
