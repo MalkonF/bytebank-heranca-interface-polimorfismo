b873198c49cbeb0a975054311daa065a6c29eb9b
MalkonF <sudo777@protonmail.com>
2021-06-24 16:09:52 -0300

    Criando novas contas - Polimorfismo e herança

--- src/main.kt
+++ src/main.kt
@@ -1,59 +1,35 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    val alex = Funcionario(
-        nome = "Malkon",
-        cpf = "111.111.111-11",
-        salario = 1000.00
+    val contaCorrente = ContaCorrente(
+        titular = "Alex",
+        numero = 1000
     )
 
-    println("Nome: ${alex.nome}")
-    println("Cpf: ${alex.cpf}")
-    println("Salário: ${alex.salario}")
-    println("Bonificação: ${alex.bonificacao}")
-
-    val fran = Gerente(
-        nome = "Fran",
-        cpf = "222.222.222-22",
-        salario = 2000.00,
-        senha = 1234
+    val contaPoupanca = ContaPoupanca(
+        titular = "Fran",
+        numero = 1001
     )
 
-    println("Nome: ${fran.nome}")
-    println("Cpf: ${fran.cpf}")
-    println("Salário: ${fran.salario}")
-    println("Bonificação: ${fran.bonificacao}")
+    contaCorrente.deposita(1000.0)
+    contaPoupanca.deposita((1000.0))
 
-    if (fran.autentica(1234)) {
-        println("Autententicou com sucesso")
-    } else {
-        println("Autenticação falhou")
-    }
-    //aqui fala que gui só poderá usar as coisas de Funcionario, por exemplo, plr não vai poder acessar porque só existe nele e nao em funcionario
-    val gui: Funcionario = Diretor(
-        nome = "Gui",
-        cpf = "333.333.333-33",
-        salario = 4000.00,
-        senha = 1234,
-        plr = 200.00
-    )
+    println("Saldo conta corrente: ${contaCorrente.saldo}")
+    println("Saldo conta poupança: ${contaPoupanca.saldo}")
 
-    println("Nome: ${gui.nome}")
-    println("Cpf: ${gui.cpf}")
-    println("Salário: ${gui.salario}")
-    println("Bonificação: ${gui.bonificacao}")
-    //println("Plr: ${gui.plr}")
-    //: Analista nao é necessário mas serve para deixar explicito que Analista herda de Funcionario
-    val maria: Analista = Analista(nome = "Maria", cpf = "444.444.444-44", salario = 3000.0)
+    contaCorrente.saca(100.0)
+    contaPoupanca.saca(100.0)
 
+    println("Saldo conta corrente após saque: ${contaCorrente.saldo}")
+    println("Saldo conta poupança após saque: ${contaPoupanca.saldo}")
 
-    val calculadora = CalculadoraBonificacao()
-    calculadora.registra(alex)
-    calculadora.registra(fran)
-    calculadora.registra(gui)
-    calculadora.registra(maria)
+    contaCorrente.transfere(150.0, contaPoupanca)
+    contaPoupanca.transfere(100.0, contaCorrente)
 
-    println("Total de bonificação: ${calculadora.total}")
+    println("Saldo conta corrente após transferencia: ${contaCorrente.saldo}")
+    println("Saldo conta poupança após transferencia: ${contaPoupanca.saldo}")
 }
+
+
 
 
 
---
+++ src/testaFuncionarios.kt
@@ -0,0 +1,55 @@
+fun testaFuncionarios() {
+    val alex = Funcionario(
+        nome = "Malkon",
+        cpf = "111.111.111-11",
+        salario = 1000.00
+    )
+
+    println("Nome: ${alex.nome}")
+    println("Cpf: ${alex.cpf}")
+    println("Salário: ${alex.salario}")
+    println("Bonificação: ${alex.bonificacao}")
+
+    val fran = Gerente(
+        nome = "Fran",
+        cpf = "222.222.222-22",
+        salario = 2000.00,
+        senha = 1234
+    )
+
+    println("Nome: ${fran.nome}")
+    println("Cpf: ${fran.cpf}")
+    println("Salário: ${fran.salario}")
+    println("Bonificação: ${fran.bonificacao}")
+
+    if (fran.autentica(1234)) {
+        println("Autententicou com sucesso")
+    } else {
+        println("Autenticação falhou")
+    }
+    //aqui fala que gui só poderá usar as coisas de Funcionario, por exemplo, plr não vai poder acessar porque só existe nele e nao em funcionario
+    val gui: Funcionario = Diretor(
+        nome = "Gui",
+        cpf = "333.333.333-33",
+        salario = 4000.00,
+        senha = 1234,
+        plr = 200.00
+    )
+
+    println("Nome: ${gui.nome}")
+    println("Cpf: ${gui.cpf}")
+    println("Salário: ${gui.salario}")
+    println("Bonificação: ${gui.bonificacao}")
+    //println("Plr: ${gui.plr}")
+    //: Analista nao é necessário mas serve para deixar explicito que Analista herda de Funcionario
+    val maria: Analista = Analista(nome = "Maria", cpf = "444.444.444-44", salario = 3000.0)
+
+
+    val calculadora = CalculadoraBonificacao()
+    calculadora.registra(alex)
+    calculadora.registra(fran)
+    calculadora.registra(gui)
+    calculadora.registra(maria)
+
+    println("Total de bonificação: ${calculadora.total}")
+}
